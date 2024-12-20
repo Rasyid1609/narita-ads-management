72d681666fad7f34a237d34d0b124422adf360be
Rasyid1609 <rasyid.mahardian@gmail.com>
2024-12-20 15:52:45 +0700

    first commit

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,11 @@
+
+export default function ApplicationLogo(props) {
+    return (
+      <img
+      {...props}
+      src="/image/narita.png"
+      alt="Logo"
+      style={{ width: '75px', height: '75px' }}
+      />
+    );
+}
